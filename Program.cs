fd354b5e5abe2581090a23cbb6bf410c42fa951f
Cihan Sari <cihansari86@gmail.com>
2017-09-11 22:17:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClothingWebClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
