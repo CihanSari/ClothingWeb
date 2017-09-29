04b03007b0e2a459ab6e486c84baed2ef7fe6509
Cihan Sari <cihansari86@gmail.com>
2017-09-29 19:38:13 +0200

    Decision is saved inside json, and shown on website, Clickable images show nice mouse pointer, Fixed bug on accessing empty json

--- js/clothingBase.js
+++ js/clothingBase.js
@@ -4,9 +4,9 @@
 window.config = {};
 (function () {
     // Parse parameters
-    var s = window.location.search;
-    var reg = /([^?&=]*)=([^&]*)/g;
-    var i = null;
+    const s = window.location.search;
+    const reg = /([^?&=]*)=([^&]*)/g;
+    let i = null;
 
     while (i = reg.exec(s)) {
         window.config[i[1]] = decodeURIComponent(i[2]);
@@ -16,11 +16,15 @@ window.config = {};
 function processPainting(evt) {
     console.log(evt);
     window.lastevent = JSON.parse(evt.data);
-    var originalImage = document.getElementById('original');
+    const originalImage = document.getElementById('original');
 
     originalImage.setAttribute("src", window.lastevent.Original);
 
-    var surrealImg = document.getElementById('surreal');
+    if (window.lastevent.json.decision !== undefined) {
+        const surrealScore = document.getElementById('surrealScore');
+        surrealScore.innerText = 'Current upvote count ' + window.lastevent.json.decision[0];
+    }
+    const surrealImg = document.getElementById('surreal');
 
     surrealImg.setAttribute("src", window.lastevent.Surreal);
     surrealImg.onmouseout = function () {
@@ -31,13 +35,19 @@ function processPainting(evt) {
     };
 
     surrealImg.onclick = function () {
-        var res = {};
+        let res = {};
         res.source = window.lastevent.paintingIdx;
         res.surrealSelected = true;
         ws.send(JSON.stringify(res));
     }
 
-    var grabcutImg = document.getElementById('grabcut');
+
+    if (window.lastevent.json.decision !== undefined) {
+        const grabcutScore = document.getElementById('grabcutScore');
+        grabcutScore.innerText = 'Current upvote count ' + window.lastevent.json.decision[1];
+    }
+
+    let grabcutImg = document.getElementById('grabcut');
 
     grabcutImg.setAttribute("src", window.lastevent.Grabcut);
     grabcutImg.onmouseout = function () {
@@ -48,97 +58,106 @@ function processPainting(evt) {
     };
 
     grabcutImg.onclick = function () {
-        var res = {};
+        let res = {};
         res.source = window.lastevent.paintingIdx;
         res.grabcutSelected = true;
         ws.send(JSON.stringify(res));
     }
 
-
-    var customImg = document.getElementById('custom');
+    if (window.lastevent.json.decision !== undefined) {
+        const customScore = document.getElementById('customScore');
+        customScore.innerText = 'Current upvote count ' + window.lastevent.json.decision[2];
+    }
+    let customImg = document.getElementById('custom');
     customImg.setAttribute("src", window.lastevent.Original);
 
+    customImg.onclick = function () {
+        let res = {};
+        res.source = window.lastevent.paintingIdx;
+        res.customSelected = true;
+        ws.send(JSON.stringify(res));
+    }
 
     document.title = window.lastevent.json.title;
 
-    var painting_properties = document.getElementById('painting_properties');
+    let painting_properties = document.getElementById('painting_properties');
     painting_properties.innerHTML = '';
-    var table = document.createElement("table");
+    let table = document.createElement("table");
     painting_properties.appendChild(table);
 
     {// Title
-        var titleRow = document.createElement("TR");
+        let titleRow = document.createElement("TR");
 
-        var titleText = document.createElement("TD");
-        var titleTextNode = document.createTextNode("Title");
+        let titleText = document.createElement("TD");
+        let titleTextNode = document.createTextNode("Title");
         titleText.appendChild(titleTextNode);
         titleRow.appendChild(titleText);
 
-        var titleValue = document.createElement("TD");
+        let titleValue = document.createElement("TD");
         titleValue.className = 'ellipsis';
-        var titleValueNode = document.createTextNode(window.lastevent.json.title);
+        let titleValueNode = document.createTextNode(window.lastevent.json.title);
         titleValue.appendChild(titleValueNode);
         titleRow.appendChild(titleValue);
 
         table.appendChild(titleRow);
     }
     {// Painter
-        var painterRow = document.createElement("TR");
+        let painterRow = document.createElement("TR");
 
-        var painterText = document.createElement("TD");
-        var painterTextNode = document.createTextNode("Painter");
+        let painterText = document.createElement("TD");
+        let painterTextNode = document.createTextNode("Painter");
         painterText.appendChild(painterTextNode);
         painterRow.appendChild(painterText);
 
-        var painterValue = document.createElement("TD");
+        let painterValue = document.createElement("TD");
         painterValue.className = 'ellipsis';
-        var painterValueNode = document.createTextNode(window.lastevent.json.Painter);
+        let painterValueNode = document.createTextNode(window.lastevent.json.Painter);
         painterValue.appendChild(painterValueNode);
         painterRow.appendChild(painterValue);
 
         table.appendChild(painterRow);
     }
     {// Gender
-        var genderRow = document.createElement("TR");
+        let genderRow = document.createElement("TR");
 
-        var genderText = document.createElement("TD");
-        var genderTextNode = document.createTextNode("Gender");
+        let genderText = document.createElement("TD");
+        let genderTextNode = document.createTextNode("Gender");
         genderText.appendChild(genderTextNode);
         genderRow.appendChild(genderText);
 
-        var genderValue = document.createElement("TD");
+        let genderValue = document.createElement("TD");
         genderValue.className = 'ellipsis';
-        var genderValueNode = document.createTextNode(window.lastevent.json.Gender);
+        let genderValueNode = document.createTextNode(window.lastevent.json.Gender);
         genderValue.appendChild(genderValueNode);
         genderRow.appendChild(genderValue);
 
         table.appendChild(genderRow);
     }
     {// Year
-        var yearRow = document.createElement("TR");
+        let yearRow = document.createElement("TR");
 
-        var yearText = document.createElement("TD");
-        var yearTextNode = document.createTextNode("Year");
+        let yearText = document.createElement("TD");
+        let yearTextNode = document.createTextNode("Year");
         yearText.appendChild(yearTextNode);
         yearRow.appendChild(yearText);
 
-        var yearValue = document.createElement("TD");
+        let yearValue = document.createElement("TD");
         yearValue.className = 'ellipsis';
-        var yearValueNode = document.createTextNode(window.lastevent.json.Year);
+        let yearValueNode = document.createTextNode(window.lastevent.json.Year);
         yearValue.appendChild(yearValueNode);
         yearRow.appendChild(yearValue);
 
         table.appendChild(yearRow);
     }
     {// Dominant color
-        var dominantColorRow = document.createElement("TR");
+        let dominantColorRow = document.createElement("TR");
 
-        var dominantColorText = document.createElement("TD");
-        var dominantColorTextNode = document.createTextNode("Color");
+        let dominantColorText = document.createElement("TD");
+        let dominantColorTextNode = document.createTextNode("Color");
         dominantColorText.appendChild(dominantColorTextNode);
         dominantColorRow.appendChild(dominantColorText);
 
-        var dominantColorValue = document.createElement("TD");
+        let dominantColorValue = document.createElement("TD");
         dominantColorValue.className = 'ellipsis';
         dominantColorValue.style.backgroundColor = "rgb(" + window.lastevent.json.DominantRed + "," + window.lastevent.json.DominantGreen + "," + window.lastevent.json.DominantBlue + ")";
         dominantColorValue.style.margin = "5px";
@@ -148,11 +167,11 @@ function processPainting(evt) {
     }
 }
 
-var ws;
+let ws;
 function queryPainting() {
     ws = new WebSocket("ws://" + location.host + ":8080/painting");
     ws.onmessage = function (evt) {
-        var fail = function (err) {
+        let fail = function (err) {
             if (err !== undefined)
             {
                 alert(err);
@@ -160,7 +179,7 @@ function queryPainting() {
             location.reload();
         };
         try {
-            var data = JSON.parse(evt.data);
+            let data = JSON.parse(evt.data);
             if (data.hasOwnProperty('json') && data.hasOwnProperty('Original')) {
                 // painting event
                 processPainting(evt);
@@ -178,7 +197,7 @@ function queryPainting() {
     };
     ws.onopen = function (evt) {
         if (jQuery.isEmptyObject(window.config)) {
-            var request = {};
+            let request = {};
             request.israndom = true;
             ws.send(JSON.stringify(request));
         } else {
